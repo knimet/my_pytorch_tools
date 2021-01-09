96125f3bedb78328a5ed997597b2747a9d5a2ce9
knimet <512274107@qq.com>
2021-01-09 11:04:24 +0800

    Add files via upload
    getIdxofSameItems: given a vector, output the indexes of the items which have the same value

---
+++ getIdxofSameItems.py
@@ -0,0 +1,15 @@
+import torch
+def getIdxofSameItems(a):
+#given a vector, output the indexes of the items which have the same value
+    co = a.unsqueeze(0)-a.unsqueeze(1)
+    uniquer = co.unique(dim=0)
+    out = []
+    for r in uniquer:
+        cover = torch.arange(a.size(0))
+        mask = r==0
+        idx = cover[mask]
+        out.append(idx)
+    return out
+a = torch.Tensor([1,1,2,3,4,5,5,5,5])
+idxs=getIdxofSameItems(a) 
+#output: [tensor([5, 6, 7, 8]), tensor([4]), tensor([3]), tensor([2]), tensor([0, 1])]
